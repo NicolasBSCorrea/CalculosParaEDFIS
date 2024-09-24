bcf0b36af80d86e17793d735841c649422a028f4
Nícolas B S Corrêa <129511108+NicolasBSCorrea@users.noreply.github.com>
2024-09-24 11:48:24 -0300

    24-09 11h39
    Bug arrumado.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var idade = document.querySelector('#idade')
-var altura = document.querySelector('#altura')
 var peso = document.querySelector('#peso')
+var altura = document.querySelector('#altura')
+var idade = document.querySelector('#idade')
+
+
 var resultado = document.querySelector('span')
 
-function calcular(){
-    resultado = (13.75 * peso.value) + (5 + altura.value) - (6.75 * idade.value) + 66.5
+function calcular() {
+    resultado.innerHTML = ((13.75 * parseInt(peso.value)) + (5 + parseInt(altura.value)) - (6.75 * parseInt(idade.value)) + (66.5))
+
 }
