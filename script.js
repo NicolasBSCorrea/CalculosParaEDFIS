ed8381a33b1367318cbb0283fbaa306cf8926a43
Nícolas B S Corrêa <129511108+NicolasBSCorrea@users.noreply.github.com>
2024-09-24 22:13:53 -0300

    24-09 22h13
    Muitos erros, retorno do bug de validação de gênero.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var masc = document.querySelector('#masc')
-var fem = document.querySelector('#fem')
+var sexo = document.getElementsByTagName('sexo').value
 
 var peso = document.querySelector('#peso')
 var altura = document.querySelector('#altura')
@@ -9,11 +8,11 @@ var idade = document.querySelector('#idade')
 var resultado = document.querySelector('span')
 
 function calcular() {
-    if(masc == masc){
+    if(sexo == 1){
         //resultado.innerHTML = ((13.75 * parseInt(peso.value)) + (5 + parseInt(altura.value)) - (6.75 * parseInt(idade.value)) + (66.5))
         resultado.innerHTML = 1
     }
-    if(fem == fem){
+    else{
         //resultado.innerHTML = ((9.56 * parseInt(peso.value)) + (1.85 + parseInt(altura.value)) - (4.68 * parseInt(idade.value)) + (65.71))
         resultado.innerHTML = 0
     }
