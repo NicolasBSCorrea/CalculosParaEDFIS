a43a8275e8e07a0f75fde4c10193cef4db82476a
Nícolas B S Corrêa <129511108+NicolasBSCorrea@users.noreply.github.com>
2024-09-24 21:37:24 -0300

    24-09 21h36
    Implementação de gênero do entrevistado para mudança de fórmula

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var masc = document.querySelector('#masc')
+var fem = document.querySelector('#fem')
+
 var peso = document.querySelector('#peso')
 var altura = document.querySelector('#altura')
 var idade = document.querySelector('#idade')
@@ -6,6 +9,14 @@ var idade = document.querySelector('#idade')
 var resultado = document.querySelector('span')
 
 function calcular() {
-    resultado.innerHTML = ((13.75 * parseInt(peso.value)) + (5 + parseInt(altura.value)) - (6.75 * parseInt(idade.value)) + (66.5))
+    if(masc == masc){
+        //resultado.innerHTML = ((13.75 * parseInt(peso.value)) + (5 + parseInt(altura.value)) - (6.75 * parseInt(idade.value)) + (66.5))
+        resultado.innerHTML = 1
+    }
+    if(fem == fem){
+        //resultado.innerHTML = ((9.56 * parseInt(peso.value)) + (1.85 + parseInt(altura.value)) - (4.68 * parseInt(idade.value)) + (65.71))
+        resultado.innerHTML = 0
+    }
+    
 
 }
